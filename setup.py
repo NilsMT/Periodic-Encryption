1d155c3aefce90fa9b55e272c23d48710a4f5999
E221936K <nilsmoreauthomas@gmail.com>
2024-12-12 02:07:25 +0100

    réparation

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from distutils.core import setup
 
 setup(
-    name="Periodic_Encryption",
-    packages = ["Periodic_Encryption"], 
+    name="periodic_encryption",
+    packages = ["periodic_encryption"], 
     version = '1.0',
     license='MIT',
 
     description="Allow you to encrypt & decrypt strings using the periodic table elements",
 
-    author="Nils Moreau--Thomas",
+    author="NilsMT",
     author_email="nilsmoreauthomas@gmail.com",
 
     url="https://github.com/NilsMT/periodic-encryption",
-    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz'
 
     install_requires = [
         'periodictable',
@@ -20,14 +19,14 @@ setup(
         'wheel',
         'pandas',
         'pytest'
-    ]
-    
+    ],
+
     keywords = ['CRYPTOGRAPHY', 'SECURITY', 'PERIODIC TABLE', 'CHEMICAL', 'WEIRD'],
   
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers, Cryptography Engineers',
-        'Topic :: Software Development :: Build Tools',
+        'Intended Audience :: Developers',
+        'Topic :: Security :: Cryptography',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
